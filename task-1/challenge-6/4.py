20e98ff2deb3dca602f1bde5c3a604d0ec29a651
cjunior <realc@protonmail.com>
2019-10-02 13:48:11 +0530

    dev 4

---
+++ task-1/challenge-6/4.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the number \n"))
+b=int(input())
+print("Sum= ",a+b)
+print("Difference =", a-b)
+print("Product =", a*b)
+print("Quotient =", a/b)
+print("Remainder =", a%b)
+if a>b:
+    print("%d is greater"%(a))
+else:
+    print("%d is greater"%(b))
